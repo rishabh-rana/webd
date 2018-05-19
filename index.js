d6ce7117d7759cdaad6fd9c3a3d65f7f78379706
Rishabh <rishabhrana36@gmail.com>
2018-05-19 14:04:35 +0530

    changed text

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 
 app.get("/", function(req, res) {
-  res.send("server running");
+  res.send("server running and deployed");
 });
 
 app.listen(process.env.PORT || 3000,process.env.IP);
