e8bfe212ff546d2bfbf11fd81a5e4a21d7d444ae
Rishabh <rishabhrana36@gmail.com>
2018-05-20 19:54:17 +0530

    changed

--- client/src/index.js
+++ client/src/index.js
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import App from "./app";
+import App from "./app.js";
 import {createStore, applyMiddleware} from "redux";
 import {Provider} from 'react-redux'
 import reducers from "./reducers/index";
