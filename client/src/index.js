8ce70201dec631d7db8c049d98c458f347bb942e
Rishabh <rishabhrana36@gmail.com>
2018-05-20 20:16:13 +0530

    change name

--- client/src/index.js
+++ client/src/index.js
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import App from "./app.js";
+import App from "./Ap";
 import {createStore, applyMiddleware} from "redux";
 import {Provider} from 'react-redux'
 import reducers from "./reducers/index";
